d2fdeea94615a6fd8fc146b20b3493597608aa22
sviat <sviatoslav.kuzbyt.v@gmail.com>
2023-11-28 18:47:18 +0200

    created project and splash activity

---
+++ app/src/main/java/com/sviatkuzbyt/library/ui/other/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.sviatkuzbyt.library.ui.other
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sviatkuzbyt.library.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
